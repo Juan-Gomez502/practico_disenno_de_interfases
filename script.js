f683b21b580a1a6c9297902f006c8a81dd3a300c
Juan-Gomez502 <jsgomezgambuzzi@escuelasproa.edu.ar>
2026-09-04 11:13:43 -0300

    cambios
    Corregi codigos

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ input.addEventListener('input', function() {
     const searchTerm = input.value.toLowerCase();
     const especies = Array.from(document.querySelectorAll('.marco'));
     
-    const elementos_filtrados =  elementos_filtados.filter(especie => 
-        const dataRaza = elemento.getAttribute('data-raza').toLowerCase();
+    const elementos_filtrados =  especies.filter(especie => {
+        const dataRaza = especie.getAttribute('data-raza').toLowerCase();
         return dataRaza.includes(searchTerm);
-    );
+    });
 
     
     especies.forEach(e => e.style.display = 'none');
